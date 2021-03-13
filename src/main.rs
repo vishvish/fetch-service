17585bd3175104ca5a448d755b164d119cdf9025
muji <muji@tmpfs.org>
2021-03-13 10:20:58 +0800

    Use Error and Result type.
    Run `cargo fmt`.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-use std::error::Error;
 use std::fs;
 use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter};
 use std::io::prelude::*;
+use std::io::{BufRead, BufReader, BufWriter};
 
-use chrono::{DateTime, Utc};
 use chrono::prelude::*;
+use chrono::{DateTime, Utc};
 use futures::StreamExt;
 use log::{error, info};
 
-fn read_lines(path: &str) -> Result<Vec<String>, Box<dyn Error + Send + Sync>> {
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Result<T> = std::result::Result<T, Error>;
+
+fn read_lines(path: &str) -> Result<Vec<String>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
-    Ok(
-        reader.lines().filter_map(Result::ok).collect()
-    )
+    Ok(reader.lines().filter_map(std::result::Result::ok).collect())
 }
 
-async fn write_file(filename: &str, data: String) -> Result<(), Box<dyn Error + Send + Sync>> {
+async fn write_file(filename: &str, data: String) -> Result<()> {
     let utc: DateTime<Utc> = Utc::now();
-    let folder: String = format!("data/{year}/{month}/{day}/", year = utc.year(), month = utc.month(), day = utc.day());
+    let folder: String = format!(
+        "data/{year}/{month}/{day}/",
+        year = utc.year(),
+        month = utc.month(),
+        day = utc.day()
+    );
     let _ = fs::create_dir_all(&folder)?;
     let mut buffer = BufWriter::new(File::create(folder + filename)?);
     buffer.write_all(data.as_bytes())?;
@@ -27,29 +32,25 @@ async fn write_file(filename: &str, data: String) -> Result<(), Box<dyn Error + 
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+async fn main() -> Result<()> {
     // the urls.txt contains a link to the latest version of every file
     let urls = read_lines("urls.txt")?;
-    let fetches = futures::stream::iter(
-        urls.into_iter().map(|url| {
-            async move {
-                match reqwest::get(&url).await {
-                    Ok(resp) => {
-                        match resp.text().await {
-                            Ok(text) => {
-                                let mut base = url.split('/');
-                                let filename = base.next_back();
-                                println!("RESPONSE: {} bytes from {}", text.len(), url);
-                                let _res = write_file(filename.unwrap(), text).await;
-                            }
-                            Err(_) => error!("ERROR reading {}", url),
-                        }
-                    }
-                    Err(_) => error!("ERROR downloading {}", url),
+    let fetches = futures::stream::iter(urls.into_iter().map(|url| async move {
+        match reqwest::get(&url).await {
+            Ok(resp) => match resp.text().await {
+                Ok(text) => {
+                    let mut base = url.split('/');
+                    let filename = base.next_back();
+                    println!("RESPONSE: {} bytes from {}", text.len(), url);
+                    let _res = write_file(filename.unwrap(), text).await;
                 }
-            }
-        })
-    ).buffer_unordered(50).collect::<Vec<()>>();
+                Err(_) => error!("ERROR reading {}", url),
+            },
+            Err(_) => error!("ERROR downloading {}", url),
+        }
+    }))
+    .buffer_unordered(50)
+    .collect::<Vec<()>>();
     info!("Waiting...");
     fetches.await;
     Ok(())
